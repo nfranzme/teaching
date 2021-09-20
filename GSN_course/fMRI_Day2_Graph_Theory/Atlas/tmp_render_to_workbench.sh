ee6344ca906e8e322032c9df9891c028b8a1fefa
nfranzme <nicolai.franzmeier@med.uni-muenchen.de>
2021-09-20 09:12:54 +0200

    update missing atlas files

---
+++ GSN_course/fMRI_Day2_Graph_Theory/Atlas/tmp_render_to_workbench.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export PATH=$PATH:/Volumes/Users/nfranzme/Custom/Software/workbench/bin_macosx64
+wb_command -cifti-convert -from-text /Volumes/Users/nfranzme/Custom/Brain_Parcellations/Schaefer_2mm/Schaefer400_Braak_assignment.txt /Volumes/Users/nfranzme/Custom/Brain_Parcellations/CBIG-0.4.12-Brain_Parcellations/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/HCP/fslr32k/cifti/Schaefer2018_400Parcels_7Networks_order.pscalar.nii /Volumes/Users/nfranzme/Custom/Brain_Parcellations/Schaefer_2mm/Schaefer400_Braak_assignment.pscalar.nii
